beb85399ca3bb1a14c869c650f9a9626b6c88b02
rdouglas <ruairidouglas@gmail.com>
2021-11-12 00:08:40 +0000

    Update test.sh

--- .github/test.sh
+++ .github/test.sh
@@ -1,17 +1,24 @@
 
-baseversion="0.0.1"
-version="0.0.1-thing.1"
-branch="thing"
-echo "${version%.*}"
-if [[ $version =~ $branch ]]
-then
-    basev="${version%.*}"
-    dotv="${version##*.}"
-    echo "${basev}.$((dotv + 1))"
-else
-    echo "${baseversion}-${branch}.1"
-fi
+# baseversion="0.0.1"
+# version="0.0.1-thing.1"
+# branch="thing"
+# echo "${version%.*}"
+# if [[ $version =~ $branch ]]
+# then
+#     basev="${version%.*}"
+#     dotv="${version##*.}"
+#     echo "${basev}.$((dotv + 1))"
+# else
+#     echo "${baseversion}-${branch}.1"
+# fi
 # foo="0.0.2-this.1"
 # bar="${foo##*.}"
 # thing="0.0.2-this.$((bar + 1))"
 # echo $thing
+
+thing=$(npm view ruairi-test dist-tags.this-bra)
+echo $thing
+if [[ $thing == "" ]]
+then
+    echo "thing"
+fi
